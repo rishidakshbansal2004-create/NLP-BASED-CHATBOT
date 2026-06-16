b6a32153d67003efd997c4fcfb3df1e67b9064ec
rishidakshbansal2004-create <rishidakshbansal2004@gmail.com>
2026-06-17 02:36:41 +0530

    moved load model() directly to app.py

--- Chatbot.py
+++ Chatbot.py
@@ -2,14 +2,11 @@ import pickle
 import json
 import random
 from preprocessing import preprocess
-with open("model.pkl","rb") as f:
-    model = pickle.load(f)
-with open("vectorizer.pkl","rb") as f:
-    vectorizer = pickle.load(f)
+
 with open("intent.json", "r") as f:
     data = json.load(f)
 
-def get_response(user_input):
+def get_response(user_input,model, vectorizer):
     clean_input= " ".join(preprocess(user_input))
     x=vectorizer.transform([clean_input])
     intent=model.predict(x)[0]
@@ -23,13 +20,17 @@ def get_response(user_input):
     return "SORRY! TRY AGAIN PLEASE"
 
 
-#FOR TESTING 
+#ONLY FOR TESTING  
 if __name__ == "__main__":
-    print("Chatbot is running! Type 'q' to exit.")
+    import pickle
+    with open("model.pkl", "rb") as f:
+        model = pickle.load(f)
+    with open("vectorizer.pkl", "rb") as f:
+        vectorizer = pickle.load(f)
+    
     while True:
         user_input = input("You: ")
-        if user_input.lower() == "q":
+        if user_input.lower() == "q": #q to exit
             break
-        response = get_response(user_input)
-        print(f"Bot: {response}")
+        print(f"Bot: {get_response(user_input, model, vectorizer)}")
 
